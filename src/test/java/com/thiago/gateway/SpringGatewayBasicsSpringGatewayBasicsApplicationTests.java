bb68fe5daf9ceed2b009922a1fb038949e866cda
barbosa-thiago <cartaparathiago@gmail.com>
2021-04-06 15:32:57 -0300

    Initial commit

---
+++ src/test/java/com/thiago/gateway/SpringGatewayBasicsSpringGatewayBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayBasicsSpringGatewayBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
